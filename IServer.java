4ab5f5eae009c5aa65cd636bf825d6189f07b78f
Antman537 <Antman537@users.noreply.github.com>
2017-10-04 22:13:18 -0600

    Updated coments
    Now with pre/post conditions

--- IServer.java
+++ IServer.java
@@ -2,39 +2,57 @@
 public interface IServer {
 	/*
 	 * logs user in and returns authToken. Throws login exceptions
+	 * @Pre: user&&password !=null && != ""
+	 * @Post: 0 < int authToken < 10000 || exception
 	 */
 	public int userLogin(String user, String password);
 	/*
 	 * Registers a new user and logs them in. Returns authToken
 	 * Throws existingUser exception
+	 * @Pre: user&&password != null && != ""
+	 * @Post: 0 < int authToken < 10000 || existingUser exception 
 	 */
 	public int registerUser(String user, String password);
 	/*
 	 * creates a new game, using authToken to determine the creator. Returns gameID
+	 * @Pre: 0 < authToken < 10000
+	 * @Post: 0 < int gameID < 10000
 	 */
 	public int newGame(int authToken);
 	/*
 	 * adds authToken user to gameID game
+	 * @Pre: 0 < authToken&&gameID < 10000
+	 * @Post: None
 	 */
 	public void join(int authToken, int gameID);
 	/*
 	 * removes authToken user from gameID game
+	 * @Pre: 0 < authToken&&gameID < 10000
+	 * @Post: None
 	 */
 	public void leave(int authToken, int gameID);
 	/*
 	 * returns a list of the join-able games on the server
+	 * @Pre: None
+	 * @Post: Object gamesList !=null && isType List<String> of gameIDs
 	 */
 	public Object openGames();
 	/*
 	 * returns a list of in-progress games authToken user is currently in
+	 * @Pre: 0 < authToken < 10000
+	 * @Post: Object gamesList !=null && isType List<String> of gameIDs
 	 */
 	public Object inProgressGames(int authToken);
 	/*
 	 * returns a list of unstarted games authToken user is currently in
+	 * @Pre: 0 < authToken < 10000
+	 * @Post: Object gamesList !=null && isType List<String> of gameIDs
 	 */
 	public Object unstartedGames(int authToken);
 	/*
 	 * flags gameID game as started. Initializes game objects for gameID
+	 * @Pre: 0 < gameID < 10000
+	 * @Post: None
 	 */
 	public void startGame(int gameID);
 
