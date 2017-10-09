7d7ba20e6224bd7b3f295c3fdf3fb3b385d91bfc
mattcprice <price.mc1x@gmail.com>
2017-10-08 21:05:26 -0600

    Changed from int to String for UUID implementation

--- IServer.java
+++ IServer.java
@@ -1,3 +1,5 @@
+package Common;
+
 
 public interface IServer {
 	/*
@@ -5,32 +7,32 @@ public interface IServer {
 	 * @Pre: user&&password !=null && != ""
 	 * @Post: 0 < int authToken < 10000 || exception
 	 */
-	public int userLogin(String user, String password);
+	public AuthToken userLogin(String user, String password);
 	/*
 	 * Registers a new user and logs them in. Returns authToken
 	 * Throws existingUser exception
 	 * @Pre: user&&password != null && != ""
 	 * @Post: 0 < int authToken < 10000 || existingUser exception 
 	 */
-	public int registerUser(String user, String password);
+	public AuthToken registerUser(String user, String password);
 	/*
 	 * creates a new game, using authToken to determine the creator. Returns gameID
 	 * @Pre: 0 < authToken < 10000
 	 * @Post: 0 < int gameID < 10000
 	 */
-	public int newGame(int authToken);
+	public GameInfo newGame(AuthToken authToken, String name);
 	/*
 	 * adds authToken user to gameID game
 	 * @Pre: 0 < authToken&&gameID < 10000
 	 * @Post: None
 	 */
-	public void join(int authToken, int gameID);
+	public void join(AuthToken authToken, String gameID);
 	/*
 	 * removes authToken user from gameID game
 	 * @Pre: 0 < authToken&&gameID < 10000
 	 * @Post: None
 	 */
-	public void leave(int authToken, int gameID);
+	public void leave(AuthToken authToken, String gameID);
 	/*
 	 * returns a list of the join-able games on the server
 	 * @Pre: None
@@ -42,18 +44,18 @@ public interface IServer {
 	 * @Pre: 0 < authToken < 10000
 	 * @Post: Object gamesList !=null && isType List<String> of gameIDs
 	 */
-	public Object inProgressGames(int authToken);
+	public Object inProgressGames(AuthToken authToken);
 	/*
 	 * returns a list of unstarted games authToken user is currently in
 	 * @Pre: 0 < authToken < 10000
 	 * @Post: Object gamesList !=null && isType List<String> of gameIDs
 	 */
-	public Object unstartedGames(int authToken);
+	public Object unstartedGames(AuthToken authToken);
 	/*
 	 * flags gameID game as started. Initializes game objects for gameID
 	 * @Pre: 0 < gameID < 10000
 	 * @Post: None
 	 */
-	public void startGame(int gameID);
+	public void startGame(String gameID);
 
 }
